c00b7d7b3b160c74e823c9193a7ee2b4309d95a5
rizakii <riza.alaudin@kii.com>
2016-12-23 16:35:01 +0900

    initial commit

---
+++ Training/Training/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Training
+//
+//  Created by syahRiza on 2016/12/23.
+//  Copyright © 2016 PENS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var text1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        text1.text = "PENS JOSS Banget"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
